0b2451a93af3f3a052a29432c3a4dd94583e7822
Ashish- Netam <netamashish04@gmail.com>
2026-08-26 09:22:54 +0530

    Initial project upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  function handleLogin(userData) {
+    setUser(userData);
+  }
+
+  function handleLogout() {
+    setUser(null);
+  }
+
+  if (!user) {
+    return <Login onLogin={handleLogin} />;
+  }
+
+  return (
+    <Dashboard
+      user={user}
+      onLogout={handleLogout}
+    />
+  );
+}
+
+export default App;
